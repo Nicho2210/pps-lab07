9ee5720ca83f39d261c1fcb8af79c877d243770d
RicciNicholas <riccinicho22@gmail.com>
2025-06-17 08:31:33 +0200

    implemented randomAI

--- src/main/scala/ex4/ConnectThree.scala
+++ src/main/scala/ex4/ConnectThree.scala
@@ -1,5 +1,8 @@
 package ex4
 
+import scala.util.Random
+import scala.io.StdIn.*
+
 object ConnectThree extends App:
   val bound = 3
   val streakToWin = 3
@@ -132,6 +135,27 @@ object ConnectThree extends App:
         if board == game.head then println()
   }
 
+  trait AI:
+    def player: Player
+    def placeNextDisk(game: Game): Game
+
+  object AI:
+
+    def randomAI(player: Player): AI = randomAIImpl(player)
+
+    private case class randomAIImpl(override val player: Player) extends AI:
+      assert(player != null)
+
+      override def placeNextDisk(game: Game): Game =
+        val currentBoard = game.lastOption.getOrElse(emptyBoard)
+        val availableCol: Seq[Int] = (0 to bound).filter(x => firstAvailableRow(currentBoard, x).isDefined)
+        if availableCol.nonEmpty then
+          val chosenX = availableCol(Random.nextInt(availableCol.size))
+          val newBoard = placeDisk(currentBoard, player, chosenX)
+          game :+ newBoard
+        else
+          game
+
   // Exercise 3: implement placeAnyDisk such that...
   println("EX 3:")
   printBoards(placeAnyDisk(List(), X))
@@ -171,3 +195,55 @@ object ConnectThree extends App:
       printBoards(g)
       println()
   }
+
+  println("EX 6 (random AI): ")
+
+  var gameVSAI: Game = Seq(emptyBoard)
+  var player: Option[Player] = None
+  while player.isEmpty do
+    println("Digitare il player (X or O):")
+    readLine() match
+      case "X" => player = Option(X)
+      case "O" => player = Option(O)
+      case _ => println("Valore non valido: digitare 'X' or 'O'")
+  var currentPlayer: Option[Int] = None
+  val randomAI = AI.randomAI(player.get.other)
+  while currentPlayer.isEmpty do
+    println("Vuoi essere il primo giocatore? (Y/N)")
+    readLine() match
+      case "Y" => currentPlayer = Option(0)
+      case "N" => currentPlayer = Option(1)
+      case _ => println("Valore non valido: digitare 'Y' or 'N'")
+  val cols = (0 to bound).toList
+  while !someoneIsWinning(gameVSAI.last) &&  gameVSAI.last.size < (bound + 1) * (bound + 1) do {
+    val currentDisks: Int = gameVSAI.last.size
+      println("Current Board:")
+      printBoards(Seq(gameVSAI.last))
+      currentPlayer.get % 2 match {
+        case 0 =>
+          var validMoveMade = false
+          while !validMoveMade do
+            println("Seleziona colonna:")
+            val selectedCol = readLine().toIntOption
+            selectedCol match {
+              case Some(col) if col >= 0 && col <= bound =>
+                val nextBoard = placeDisk(gameVSAI.last, player.get, col)
+                if gameVSAI.last != nextBoard then
+                  gameVSAI = gameVSAI :+ nextBoard
+                  validMoveMade = true
+                else
+                  println("Selezione non valida")
+              case _ => println(s"Input non valido. Digitare un numero tra 0 e $bound")
+            }
+        case 1 =>
+          gameVSAI = randomAI.placeNextDisk(gameVSAI)
+      }
+    currentPlayer = Option((currentPlayer.get + 1) % 2)
+  }
+  if gameVSAI.last.size == (bound + 1) * (bound + 1) then
+    println("It-s a draw")
+  else
+    val winner = if ((currentPlayer.get + 1) % 2) == 0 then player.get else player.get.other
+    println(s"$winner is the winner")
+  println("Final Board:")
+  printBoards(Seq(gameVSAI.last))
